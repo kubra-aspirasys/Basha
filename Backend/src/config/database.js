13f24be4f633ce918a39562a11791072c9a97cfa
kubra-aspirasys <kubra@aspirasys.in>
2026-02-03 13:22:10 +0530

    db update

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -5,26 +5,26 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Kubra222',
-    database: process.env.DB_NAME || 'basha_biryani_db',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USER || 'aspirfxc_basha',
+    password: process.env.DB_PASSWORD || 'basha@12345',
+    database: process.env.DB_NAME || 'aspirfxc_basha_db',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
     logging: console.log,
   },
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Kubra222',
-    database: process.env.DB_NAME_TEST || 'basha_biryani_test',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USER || 'aspirfxc_basha',
+    password: process.env.DB_PASSWORD || 'basha@12345',
+    database: process.env.DB_NAME_TEST || 'aspirfxc_basha_db',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
     logging: false,
   },
   production: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Kubra222',
-    database: process.env.DB_NAME || 'basha_biryani_prod',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USER || 'aspirfxc_basha',
+    password: process.env.DB_PASSWORD || 'basha@12345',
+    database: process.env.DB_NAME || 'aspirfxc_basha_db',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
     logging: false,
     pool: {
